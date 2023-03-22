754af3c35415d4f94d8148fee7dac6e2d6a7f955
utsavipatel86 <122020138+utsavipatel86@users.noreply.github.com>
2023-03-22 09:40:46 +0530

    Add files via upload

---
+++ day1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hello from './hello';
+function App() {
+  return (
+    <div>
+       <h1>This is App</h1>
+       <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ day1/src/hello.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Hello() {
+    return (<h1>This is Hello World!</h1>)
+}
+export default Hello;
